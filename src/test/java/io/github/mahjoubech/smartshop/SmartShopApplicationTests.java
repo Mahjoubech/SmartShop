949565a436d87570ba505ebf3cf38f0eb2de5770
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-24 12:06:59 +0100

    first commit instalation project spring boot version 4.0.0

---
+++ src/test/java/io/github/mahjoubech/smartshop/SmartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mahjoubech.smartshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
