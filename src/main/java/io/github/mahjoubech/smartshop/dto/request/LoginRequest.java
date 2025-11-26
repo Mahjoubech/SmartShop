0a86366622243cd92e18eb28dd5a4c248a856900
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-26 22:41:54 +0100

    SMRTSHP-10 Ajouter le système d'authentification avec gestion des sessions : création des contrôleurs, services et exceptions

--- src/main/java/io/github/mahjoubech/smartshop/dto/request/LoginRequest.java
+++
@@ -1,4 +0,0 @@
-package io.github.mahjoubech.smartshop.dto.request;
-
-public class LoginRequest {
-}
---
+++ src/main/java/io/github/mahjoubech/smartshop/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package io.github.mahjoubech.smartshop.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
