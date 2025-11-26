f495ee77e8bc99ad8239081fb82bb20c07d94737
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-26 22:33:26 +0100

    SMRTSHP-49 creer class AuditBaseEntity :

--- src/main/java/io/github/mahjoubech/smartshop/model/entity/BaseEntity.java
+++ src/main/java/io/github/mahjoubech/smartshop/model/entity/BaseEntity.java
@@ -1,4 +1,30 @@
 package io.github.mahjoubech.smartshop.model.entity;
 
-public class BaseEntity {
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Data
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @CreatedDate
+    @Column(name = "createAt", nullable = false, updatable = false)
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "modifyAt")
+    private LocalDateTime modifyAt;
 }
