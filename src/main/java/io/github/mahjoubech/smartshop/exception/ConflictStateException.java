4b03b6f49785035bc809dd2f2feae747a51c763c
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-11-27 17:26:14 +0100

    SMRTSHP-50 handeler Exceptions pour validations Requests

---
+++ src/main/java/io/github/mahjoubech/smartshop/exception/ConflictStateException.java
@@ -0,0 +1,7 @@
+package io.github.mahjoubech.smartshop.exception;
+
+public class ConflictStateException extends RuntimeException {
+    public ConflictStateException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/github/mahjoubech/smartshop/model/enums/CustomerTierStatus.java
@@ -0,0 +1,8 @@
+package io.github.mahjoubech.smartshop.model.enums;
+
+public enum CustomerTierStatus {
+    BASIC,
+    SILVER,
+    GOLD,
+    PLATINUM;
+}
