3c840cb90dc82c5ed7847fdeb43b8adf484b75a0
Mahjoubech <charkaouielmahjoub50@gmail.com>
2025-12-02 19:09:49 +0100

    SMRTSHP-54 createOrder response not returning full OrderResponseDetailDTO /: refactor order management: enhance createOrder response and add OrderResponseDetailDTO mapping

--- src/main/java/io/github/mahjoubech/smartshop/mapper/OrderMapper.java
+++ src/main/java/io/github/mahjoubech/smartshop/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package io.github.mahjoubech.smartshop.mapper;
 
 import io.github.mahjoubech.smartshop.dto.request.OrderRequestDTO;
+import io.github.mahjoubech.smartshop.dto.response.detail.OrderResponseDetailDTO;
 import io.github.mahjoubech.smartshop.model.entity.Order;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,4 +11,6 @@ public interface OrderMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "orderItems", ignore = true)
     Order toEntity(OrderRequestDTO dto);
+
+    OrderResponseDetailDTO toResponseDetail(Order entity);
 }
